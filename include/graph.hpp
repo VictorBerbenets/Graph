d294d5a7ed793328277157ff446eeba9d5023e58
Victor Berbenets <berbenets.vitia@yandex.ru>
2024-01-21 19:32:06 +0300

    made initializer list constructor

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 #include <concepts>
 #include <optional>
 #include <utility>
 #include <initializer_list>
-#include <unordered_set>
+#include <unordered_map>
 
 namespace yLAB {
 
@@ -26,30 +27,71 @@ class Graph: public IGraph<T> {
   using size_type     = std::size_t;
   using value_type    = T;
   using search_type   = IGraph<T>::search_type;
-  using vertices_pair = std::pair<T, T>;
+  using vertices_pair = std::pair<value_type, value_type>;
+  using vertices_map  = std::unordered_map<value_type, size_type>;
 
-  static constexpr size_type NLine = 4;
+  static constexpr size_type NLine = 3; // table lines
  public:
-  Graph() = default;
+  constexpr Graph() = default;
   Graph(std::initializer_list<vertices_pair> ls) {
-    std::unordered_set<value_type> vertices;
-    auto insert_if = [&vertices](const value_type &vertex) {
+    vertices_map vertices;
+    std::vector<value_type> save_order;
+    auto insert_if = [&vertices, &save_order](const value_type &vertex,
+                                              size_type &vert_id) {
       if (vertices.find(vertex) == vertices.end()) {
-        vertices.insert(vertex);
+        save_order.push_back(vertex);
+        vertices.insert({vertex, vert_id});
+        ++vert_id;
       }
     };
-    for (auto &&[v1, v2] : ls) {
-      insert_if(v1);
-      insert_if(v2);
+
+    for (size_type vert_id = 0; auto &&[v1, v2] : ls) {
+      insert_if(v1, vert_id);
+      insert_if(v2, vert_id);
     }
-    auto edges_num = ls.size() * 2;
-    table_.reserve((vertices.size() + edges_num) * NLine);
+
+    nvertices_    = vertices.size();
+    table_offset_ = nvertices_ + ls.size() * 2;
+    table_.assign(table_offset_ * NLine, value_type{0});
+
+    
+    fill_first_table_part(ls);
+    fill_second_table_part(vertices, save_order);
   }
 
   search_type dfs() const override {}
   search_type bfs() const override {}
+
  private:
-  std::vector<T> table_;
+  void fill_first_table_part(std::initializer_list<vertices_pair> ls) {
+    for (size_type id = nvertices_; auto &&[v1, v2] : ls) {
+      table_[id++] = v1;
+      table_[id++] = v2;
+    }
+  }
+
+  void fill_second_table_part(vertices_map &vert_data,
+                          const std::vector<value_type> &order) {
+    auto map_copy = vert_data;
+    for (size_type curr_id = nvertices_; curr_id < table_offset_; ++curr_id) {
+      auto vertex = table_[curr_id];
+      table_[vert_data[vertex] + table_offset_] = curr_id;
+      table_[curr_id + table_offset_] = map_copy[vertex];
+      table_[curr_id + table_offset_ * 2] = vert_data[vertex];
+      vert_data[vertex] = curr_id;
+    }
+
+    for (size_type table_id = 0, offset = table_offset_ * 2;
+                                 table_id < nvertices_; ++table_id) {
+      table_[table_id + offset] = vert_data[order[table_id]];
+    }
+  }
+
+
+ private:
+  std::vector<value_type> table_;
+  size_type table_offset_;
+  size_type nvertices_;
 };
 
 } // <--- namespace yLAB
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 
- yLAB::Graph<int> g{{1,2}, {2,3}};
+ yLAB::Graph<int> g{{1,2}, {1, 3}, {2,3}, {2, 4}, {3, 4}};
 
 }
 
