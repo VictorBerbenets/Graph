5ca2ed7c00be3c17a4c0b59ab0f331a831373669
Victor Berbenets <berbenets.vitia@yandex.ru>
2024-03-18 10:00:46 +0300

    changed test runner work, it only compares now

--- tests/end2end/test_runner.sh
+++ tests/end2end/test_runner.sh
@@ -17,72 +17,60 @@ checker="./tests/checker"
 
 ##------------------------------------------------------------------------------------------##
 function run_tests {
-
-    echo -en "${white} generating tests ... ${usual}"
-    generate_tests
-    printf "\n"
-    echo -en "${white} done ${usual}"
-    printf "\n"
-
     touch compare_file.txt
     npassed_count=0
-    if [ -d "${resource_dir}" ]
-    then
-        echo -e "${white}---end2end testing---${usual}"
-        for ((i = 1; i <= ${tests_number}; ++i))
-        do
-                tmp_check="${tests_dir}test${i}.txt"
-                ans_file="${answs_dir}ans${i}.txt"
+    echo -e "${white}---end2end testing---${usual}"
+    for ((i = 1; i <= ${tests_number}; ++i))
+    do
+      tmp_check="${tests_dir}test${i}.txt"
+      ans_file="${answs_dir}ans${i}.txt"
 
-                echo -n -e "${purple}Test ${i}: ${usual}\n"
-                ${checker} ${tmp_check} ${ans_file} > compare_file.txt
-                if [ "$(cat compare_file.txt)" == "passed" ]
-                then
-                    echo -e "${green}passed${usual}"
-                else
-                    echo -e "${red}failed${usual}"
-                    npassed_count=$((${npassed_count} + 1))
-                fi
-                echo -en "${blue}painted graph:${usual}"
-                printf "\n"
-                cat ${ans_file}
-                printf "\n"
-        done
-    else
-        echo -e "${blue}can't generate tests. ${usual}${resource_dir} is empty"
-    fi
+      echo -n -e "${purple}Test ${i}: ${usual}\n"
+      ${checker} ${tmp_check} ${ans_file} > compare_file.txt
+      if [ "$(cat compare_file.txt)" == "passed" ]
+      then
+          echo -e "${green}passed${usual}"
+      else
+          echo -e "${red}failed${usual}"
+          npassed_count=$((${npassed_count} + 1))
+      fi
+      echo -en "${blue}painted graph:${usual}"
+      printf "\n"
+      cat ${ans_file}
+      printf "\n"
+    done
     echo -e "${white}-------------------------------------Total----------------------------------${usual}"
     if [ ${npassed_count} -eq 0 ]
     then
-        echo -e "${blue} \t\t\t\tAll tests passed! ${usual}"
+      echo -e "${blue} \t\t\t\tAll tests passed! ${usual}"
     elif [ ${npassed_count} -eq 1 ]
     then
-        echo -e "${red} \t\t\t\t ${npassed_count} test failed( ${usual}"
+      echo -e "${red} \t\t\t\t ${npassed_count} test failed( ${usual}"
     elif [ ${npassed_count} -gt 1 ]
     then
-        echo -e "${red} \t\t\t\t ${npassed_count} tests failed( ${usual}"
+      echo -e "${red} \t\t\t\t ${npassed_count} tests failed( ${usual}"
     fi
 
     rm compare_file.txt
 }
 ##------------------------------------------------------------------------------------------##
 
-##------------------------------------------------------------------------------------------##
-function generate_tests {
-    ${exe_test_file} ${tests_number}
-}
-##------------------------------------------------------------------------------------------##
-
 #main part
+
 error="false"
 tests_number=0
 
-if [ $# -eq 1 ]
+if [ $# -eq 3 ]
 then
-    tests_number=$1
-else
+  tests_number=$1
+  tests_dir=$2
+  answs_dir=$3;
+elif [ $# -ne 1 ]
+then
     error="true"
-    echo -e "${red}invalid number of arguments: expected 1, got $#"
+    echo -e "${red}invalid number of arguments: expected 3 or 1, got $#"
+else
+  tests_number=$1
 fi
 
 if [ $error = "false" ]
